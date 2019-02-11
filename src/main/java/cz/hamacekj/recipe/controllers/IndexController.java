b5af212286d196f034eb77f2deb5c694195085ab
Jan Hamáček <hamacekh@gmail.com>
2019-02-11 17:35:06 +0100

    Created index page.

---
+++ src/main/java/cz/hamacekj/recipe/controllers/IndexController.java
@@ -0,0 +1,14 @@
+package cz.hamacekj.recipe.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"", "/index", "/index.html"})
+    public String getIndex(){
+        return "index";
+    }
+
+}
